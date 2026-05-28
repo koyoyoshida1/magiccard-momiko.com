29ee5253b7bc95fbadc669b681c4464aac462fab
Kurehachan <yoshida.k.bs@m.titech.ac.jp>
2026-05-28 10:00:28 +0900

    Add Thea Decia PvP guide

--- script.js
+++ script.js
@@ -462,7 +462,7 @@ function playSonixVideo(button) {
 }
 
   document.addEventListener('click', function (e) {
-    const button = e.target.closest('.flas-youtube-lite');
+    const button = e.target.closest('.flas-youtube-lite, .decia-youtube-lite');
     if (!button) return;
 
     const videoId = button.dataset.youtubeId;
@@ -470,7 +470,7 @@ function playSonixVideo(button) {
 
     const iframe = document.createElement('iframe');
     iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&start=${start}&rel=0&modestbranding=1`;
-    iframe.title = 'フラス護衛隊 解説動画';
+    iframe.title = button.getAttribute('aria-label') || '解説動画';
     iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
     iframe.allowFullscreen = true;
 
